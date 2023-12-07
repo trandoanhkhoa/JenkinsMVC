958a3f2bc0113eee16af964699156328bbe4f166
Anh Khoa <trandoanhkhoa192837@gmail.com>
2023-12-07 16:44:25 +0700

    commit

---
+++ JenkinsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenkinsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Hoc Jenkins cung khoa
